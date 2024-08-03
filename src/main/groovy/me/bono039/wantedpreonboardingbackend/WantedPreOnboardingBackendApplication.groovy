670a3af3f0dcbcd0db65b4ac2d1d5674fe031980
bono039 <bono039@hanyang.ac.kr>
2024-08-04 00:42:45 +0900

    first commit

---
+++ src/main/groovy/me/bono039/wantedpreonboardingbackend/WantedPreOnboardingBackendApplication.groovy
@@ -0,0 +1,13 @@
+package me.bono039.wantedpreonboardingbackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WantedPreOnboardingBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WantedPreOnboardingBackendApplication, args)
+	}
+
+}
---
+++ src/test/groovy/me/bono039/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.groovy
@@ -0,0 +1,13 @@
+package me.bono039.wantedpreonboardingbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WantedPreOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
